0c1998fc966e066c24ede6e4f78f038862737f99
Adebola Omoboya <adeomoboya@gmail.com>
2023-05-06 16:38:32 +0100

    First Commit

---
+++ src/main/java/io/factorialsystems/msscstoreproducts/MsscStoreProductsApplication.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscstoreproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscStoreProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscStoreProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/factorialsystems/msscstoreproducts/MsscStoreProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscstoreproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscStoreProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
